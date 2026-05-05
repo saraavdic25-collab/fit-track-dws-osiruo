ca6565815f1ccfee5ed5012504be800309871a4e
Sara Avdić <sara.avdic.25@size.ba>
2026-05-05 22:03:25 +0200

    Zavrsena Faza 1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-10 bg-white rounded-xl shadow-2xl">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Fit Track Projekat 
+        </h1>
+        <p className="mt-4 text-gray-600">
+          Faza 1 je uspješno završena. Spremna si za pravljenje stranica!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
